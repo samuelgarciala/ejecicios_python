f687929f4b323623b69c06189de50dbaca43ed15
Samuel <samuelgarciala@gmail.com>
2025-01-20 20:35:35 +0100

    remove ejercicio3.py

--- ejecicio3.py
+++
@@ -1,9 +0,0 @@
-#Contar en un diccionario cuanto se repiten los carateres de un string
-
-cordenadas = {"x" : ["3", "5", "6"],"y":["1", "4", "5"],"z":["0", "4", "7"]}
-
-
-lista_de_valores = qlist(cordenadas.values)
-
-
-print (lista_de_valores)
