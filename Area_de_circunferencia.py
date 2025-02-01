e8dcfbb6daf087accf0afec5820be962ea534ed3
Samuel <samuelgarciala@gmail.com>
2025-02-01 11:09:21 +0100

    refactor(python) se ha modulado el ejercicio de circunferencia usando funciones

--- Area_de_circunferencia.py
+++ Area_de_circunferencia.py
@@ -1,6 +1,28 @@
 #3- Diseñar una aplicación que calcule la longitud y el área de una circunferencia.
-print("ingrese radio:")
-radio=int(input())
-logitud= 2 * 3.14 * radio
-area= 3.14 * radio ** 2
-print(f"el area es {area} y la logitud es {logitud}")
+def calcular_area_circunferencia(radio):
+    area= 3.14 * radio ** 2
+    return area
+
+def calcular_longitud_circuferencia(radio):
+    logitud= 2 * 3.14 * radio
+    return logitud
+
+def entrada_de_datos():
+        print("ingrese radio:")
+        radio=int(input())
+        return radio
+
+def salida_de_datos(area,longitud):
+    print(f"el area es {area} y la logitud es {longitud}")
+
+def main():
+    r = entrada_de_datos()
+    area = calcular_area_circunferencia(r)
+    longitud =calcular_longitud_circuferencia(r)
+    salida_de_datos(area,longitud)
+
+main()
+
+
+
+
